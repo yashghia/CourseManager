eda6a175466461bacc39b552d9f571fa7bb9e875
Yash Ghia <yghia@uncc.edu>
2017-11-01 13:11:57 -0400

    Initial framework

---
+++ app/src/main/java/com/example/yash/hw6/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.yash.hw6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.home:
+                return true;
+            case R.id.instructors:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
